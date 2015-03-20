2129548ba047c8ca732fc64af6c3217d379b54cc
Newms34 <Newms3450@msn.com>
2015-03-20 15:36:01 -0400

    model edits (prev/next)

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,15 +5,24 @@ var Quest = require('./quest.js')
 var schema = new mongoose.Schema({
 
     levels: Number,
-    ownedItems:[String],
-    created:[{type: mongoose.Schema.Types.ObjectId, ref: 'Quest'}],
-    participating:[{type: mongoose.Schema.Types.ObjectId, ref: 'Quest'}],
-    pastQuests:[{
-        questId: {type: mongoose.Schema.Types.ObjectId, ref: 'Quest'},
-        points:Number,
-        stepsPurchased:[Number]
+    ownedItems: [String],
+    created: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quest'
     }],
-    
+    participating: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quest'
+    }],
+    pastQuests: [{
+        questId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Quest'
+        },
+        points: Number,
+        stepsPurchased: [Number]
+    }],
+
     google: {
         id: String,
         name: String,
@@ -23,18 +32,18 @@ var schema = new mongoose.Schema({
 
 // generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
 // are all used for local authentication security.
-var generateSalt = function () {
+var generateSalt = function() {
     return crypto.randomBytes(16).toString('base64');
 };
 
-var encryptPassword = function (plainText, salt) {
+var encryptPassword = function(plainText, salt) {
     var hash = crypto.createHash('sha1');
     hash.update(plainText);
     hash.update(salt);
     return hash.digest('hex');
 };
 
-schema.pre('save', function (next) {
+schema.pre('save', function(next) {
 
     if (this.isModified('password')) {
         this.salt = this.constructor.generateSalt();
@@ -48,7 +57,7 @@ schema.pre('save', function (next) {
 schema.statics.generateSalt = generateSalt;
 schema.statics.encryptPassword = encryptPassword;
 
-schema.method('correctPassword', function (candidatePassword) {
+schema.method('correctPassword', function(candidatePassword) {
     return encryptPassword(candidatePassword, this.salt) === this.password;
 });
 
