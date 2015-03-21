20ce3452f59a118ae267aa7cbb5490642205dfca
Sarah Muenzinger <sarah.muenzinger@gmail.com>
2015-03-21 16:33:35 -0400

    front end routing added to QuestFactory

--- browser/js/common/factories/QuestFactory.js
+++ browser/js/common/factories/QuestFactory.js
@@ -3,7 +3,17 @@ app.factory('QuestFactory', function($http){
 
 	return {
 		getAllQuests: function() {
-			return $http.get('/quests').then(function(res) {
+			return $http.get('/api/quests').then(function(res) {
+				return res.data;
+			});
+		},
+		getCreatedQuestsByID: function(creatorID){
+			return $http.get('/api/quests/creator/'+creatorID).then(function(res) {
+				return res.data;
+			});
+		},
+		getJoinedQuestsById: function(userID){
+			return $http.get('/api/quests/participant/'+userID).then(function(res) {
 				return res.data;
 			});
 		}
