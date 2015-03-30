c88a7e316e2fa28ed91a2353c5ae0d16adca1969
sautille <wyvonne@gmail.com>
2015-03-29 23:53:33 -0400

    Modified icon. Working on extension redirect.

--- Extension/js/application/states/step/step.js
+++ Extension/js/application/states/step/step.js
@@ -18,22 +18,28 @@ app.controller('StepCtrl', function ($scope, QuestFactory, UserFactory, $state) 
 			$scope.chosenQuest = popUser.participating[$scope.participatingIndex];
 			$scope.stepId = popUser.participating[$scope.participatingIndex].currentStep;
 		// 	console.log("step we send", $scope.stepId)
-			QuestFactory.getStepById($scope.stepId).then(function(data){
+			QuestFactory.getStepById($scope.stepId).then(function (data) {
 				$scope.step = data;
 			})
 		})
 	});
-	$scope.launchReading = function(){
+	$scope.launchReading = function() {
 		chrome.tabs.create({url: "http://"+$scope.step.url});
 	}
-	$scope.submit = function(){
+
+	chrome.extension.sendRequest({redirect: "http://www.google.com"});
+	chrome.runtime.sendMessage(string extensionId, any message, object options, function (res) {
+		console.log(res);
+	})
+
+	$scope.submit = function() {
 		//will verify that the answer is correct
 		//if so will update current step to be the next step
 		//and send user to success page
 		if($scope.step.qType == "Fill-in"){
 			console.log("correct question type")
 			if($scope.userAnswer == $scope.step.fillIn){
-				UserFactory.addPoints($scope.stepId).then(function(data){
+				UserFactory.addPoints($scope.stepId).then(function (data) {
 					UserFactory.changeCurrentStep($scope.stepId);
 					$state.go('success');
 				})
--- Extension/js/background.js
+++ Extension/js/background.js
@@ -1,7 +1,13 @@
-chrome.extension.onConnect.addListener(function(port) {
+chrome.extension.onConnect.addListener(function (port) {
 	console.log("this connected!");
-	port.onMessage.addListener(function(message) {
+	port.onMessage.addListener(function (message) {
 		console.log('message received', message);
 		port.postMessage('Howdy');
 	});
+});
+
+chrome.extension.onRequest.addListener(function (request, sender) {
+	console.log("request", request);
+	console.log("sender", sender);
+    chrome.tabs.update(sender.tab.id, {url: request.redirect});
 });
