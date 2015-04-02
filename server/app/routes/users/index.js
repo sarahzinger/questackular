d13d41f929c20b802b89f0c235aa04ab0a2ccb59
sautille <wyvonne@gmail.com>
2015-04-02 17:47:51 -0400

    ok

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -102,37 +102,7 @@ router.get('/:id', function(req, res, next) {
                 console.log('STEPS THINGS', userFullyPopulated)
                 res.json(userFullyPopulated);
             });
-            // }
-            // res.json(userInfo);
-
-            // MAYBE??? eventually we will need to do async stuff so that both "participating" and "pastQuests"
-            //      async.parallel([
-            //        function(done) {
-            //          if (userInfo.participating.length) {
-            //            console.log("running participating");
-            //         mongoose.model('User').populate(userInfo, 'participating.questId', function (err, userI) {
-            //          if (err) return res.json(err);
-            //          console.log("ran participating");
-            //         });  
-            //          }
-            //          done();
-            //          console.log("not / after running participating.questid populate");
-            //        },
-            //        function(done) {
-            //          if (userInfo.pastQuests.length) {
-            //            console.log("running pastQuests");
-            //         mongoose.model('User').populate(userInfo, 'pastQuests.questId', function (err, singleUser) {
-            //          if (err) return res.json(err);
-            //          console.log("ran pastQuests");
-            //         });
-            //          }
-            //          done();
-            //          console.log("not / after running userinfo.questid populate");
-            //        }
-            // ], function(err, results) {
-            //  console.log("success?", results);
-            //  res.json(results);
-            // });
+            
         });
 
 });
