b3a808c23aac02e0d8a24d1b91beac48896beb38
Newms34 <Newms3450@msn.com>
2015-04-02 09:54:19 -0400

    would u like fries with that

--- server/app/routes/store/index.js
+++ server/app/routes/store/index.js
@@ -32,7 +32,7 @@ router.post('/buy/', function(req, res, next) {
     if (!req.user.pointsSpent) {
         req.user.pointsSpent = 0;
     }
-
+    console.log(chalk.bgGreen.black('$ (1) $')+'x'+itemCost);
     req.user.itemsBought.push(itemId);
     req.user.pointsSpent += itemCost;
     req.user.save(function(err, resp) {
